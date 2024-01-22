e29307d1e84c810d950e619004cb09739af9b9c3
Katherine Fry <katgfry@me.com>
2024-01-22 15:47:54 -0500

    finished 1: option1

--- 1-mergeNoDupes.js
+++ 1-mergeNoDupes.js
@@ -12,21 +12,28 @@ mergeNoDupes([6, 7, 8], [7, 8, 9]) -> [6, 7, 8, 9]
 */
 
 // input: multiple arrays 
-// output: new array representing. union. of arrays (numbers in order) without duplicates
+// output: new array representing union of arrays without duplicates
+
+/* OPTION 1: more inelegant solution (naive approach) */
 const mergeNoDupes = (...arrays) => {
   // declare a constant unified initialized to an empty array 
-
+  const unified = [];
   // loop over the arrays
-
-    // if the element at the array position doesn't already exist in the output array, add it 
-
+  for (const array of arrays) {
+    // then check elements in inner array 
+    for (const el of array) {
+      // if the element at the array position doesn't already exist in the output array, add it 
+      if (!unified.includes(el)) unified.push(el); 
+    }
+  }
   // return the unified array 
-
+  return unified;
 }
-
 
 /* //TESTS:
 console.log(mergeNoDupes([1, 3, 7], [2, 3, 9], [9, 13])) // -> [1, 3, 7, 2, 9, 13]
 console.log(mergeNoDupes([4, 3])) // -> [4, 3]
-console.log(mergeNoDupes([6, 7, 8], [7, 8, 9])) // -> [6, 7, 8, 9]
-*/
+console.log(mergeNoDupes([6, 7, 8], [7, 8, 9])) // -> [6, 7, 8, 9] */
+
+
+/* OPTION 2: using a set */
