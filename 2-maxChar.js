8d8072cee426d755ac0a8fd0909d72bd3f96a2ed
Katherine Fry <katgfry@me.com>
2024-01-23 11:45:49 -0500

    2: finished option2

--- 2-maxChar.js
+++ 2-maxChar.js
@@ -10,17 +10,17 @@ maxChar('abbcc') -> 'b'
 
 // input: string
 // output: string
-const maxChar = char => {
+const maxChar = str => {
   // edge case - if the passed-in char isn't a string, return 'parameter must be a string'
-  if (typeof char !== 'string') return 'parameter must be a string';
+  if (typeof str !== 'string') return 'parameter must be a string';
   // declare a const count initialized to an empty object
   const count = {};
   // loop over the passed in string
-  for (let i = 0; i < char.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     // if the char doesn't exist as a key in obj, add it, assigning it to 1 
-    if (!count[char[i]]) count[char[i]] = 1;
+    if (!count[str[i]]) count[str[i]] = 1;
     // if it does exist, increment it 
-    else count[char[i]]++;
+    else count[str[i]]++;
   }
 
   // check to see the index if there are duplicates
@@ -47,11 +47,45 @@ const maxChar = char => {
 console.log(maxChar('good morning')); // -> 'o'
 console.log(maxChar('abbcc')); // -> 'b'
 console.log(maxChar(123445)); // -> 'parameter must be a string'
- */
+*/
 
 
 /* OPTION 2: using a Map */
 
-const maxChar2 = char => {
-  
+const maxChar2 = str => {
+  // edge case - if the passed-in char isn't a string, return 'parameter must be a string'
+  if (typeof str !== 'string') return 'parameter must be a string';
+  // declare a const initialized to a new instance of a Map constructor
+  const charMap = new Map();
+  // iterate over the passed-in string 
+  for (const char of str) {
+    // if the char doesn't exist in the map yet,  add it (use .has and .set)
+    // .set uses parameters key and value, so 0 here to initialize it to 0 
+    if (!charMap.has(char)) charMap.set(char, 0);
+    // otherwise increment value by 1
+    charMap.set(char, charMap.get(char) + 1); 
+  }
+
+  // find max occurring character
+  let maxCharCandidate;
+  let maxFrequency = 0; 
+
+  // iterate over the char and freq of the map
+  for (let [char, freq] of charMap.entries()) {
+    // if freq is greater than max Frequency
+    if (freq > maxFrequency) {
+      // reassign maxCharCandidate to char
+      maxCharCandidate = char;
+      // reassign maxFrequency to freq 
+      maxFrequency = freq;
+    }
+  }
+  // return maxCharCandidate
+  return maxCharCandidate;
 }
+
+/* //TESTS: 
+console.log(maxChar2('good morning')); // -> 'o'
+console.log(maxChar2('abbcc')); // -> 'b'
+console.log(maxChar2(123445)); // -> 'parameter must be a string'
+ */
