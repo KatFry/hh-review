79e12781e9cb7a5f789c511e333794ba5cc7a8c7
Katherine Fry <katgfry@me.com>
2024-01-23 11:28:14 -0500

    2: pseudocode and strategy

---
+++ 2-maxChar.js
@@ -0,0 +1,26 @@
+/* Given a string, return the character that has the maximum frequency.
+If multiple characters occur the max number of times, return the character that occurs first. 
+
+Example:
+maxChar('good morning') -> 'o'
+maxChar('abbcc') -> 'b'
+*/
+
+// input: string
+// output: string
+const maxChar = char => {
+  // edge case - if the passed-in char isn't a string, return 'parameter must be a string'
+  
+  // declare a const count initialized to an empty object
+
+  // loop over the passed in string
+
+    // if the char doesn't exist as a key in obj, add it, assigning it to 1 
+
+    // if it does exist, increment it 
+
+    // check to see the index if there are duplicates
+
+  // return the value associated with the maxChar from the obj
+
+}
