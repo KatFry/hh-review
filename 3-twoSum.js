d8deaa5a1d4ab60b2a8af722f75ca53276c19376
Katherine Fry <katgfry@me.com>
2024-01-24 16:12:44 -0500

    3: option1 done

--- 3-twoSum.js
+++ 3-twoSum.js
@@ -11,13 +11,29 @@ twoSum([2, 4, 5], 8) // -> false
 twoSum([5, -1, -2, 3], 1) // -> true, [2, 3]
 */
 
-/* OPTION 1: naive solution  */
+/* OPTION 1: naive solution - O(n^2)  */
 
 // input: array, target number
 // output: boolean 
 const twoSum1 = (arr, target) => {
-  
+  // iterate over the passed-in array 
+  for (let i = 0; i < arr.length; i++) {
+    // loop again to check every integer after it and check the sum 
+    for (let j = i + 1; j < arr.length; j++) {
+      // if the sum of the two equals the target, return true 
+      if (arr[i] + arr[j] === target) return true;
+    }
+  }
+  // return false otherwise 
+  return false;
 }
+
+/* // TESTS: 
+console.log(twoSum1([2, 4, 5], 9)); // -> true, [1, 2]
+console.log(twoSum1([2, 4, 5], 8)); // -> false 
+console.log(twoSum1([5, -1, -2, 3], 1)); // -> true, [2, 3]
+ */
+
 
 /* OPTION 2: better solution */
 const twoSum2 = (arr, target) => {
@@ -25,9 +41,22 @@ const twoSum2 = (arr, target) => {
 }
 
 
+/* // TESTS: 
+console.log(twoSum2([2, 4, 5], 9)); // -> true, [1, 2]
+console.log(twoSum2([2, 4, 5], 8)); // -> false 
+console.log(twoSum2([5, -1, -2, 3], 1)); // -> true, [2, 3]
+ */
+
 /* OPTION 3: returning indices instead */
 // input: array, target number
 // output: array of indices of numbers that added to target number 
 const twoSum3 = (arr, target) => {
 
 }
+
+
+/* // TESTS: 
+console.log(twoSum3([2, 4, 5], 9)); // -> true, [1, 2]
+console.log(twoSum3([2, 4, 5], 8)); // -> false 
+console.log(twoSum3([5, -1, -2, 3], 1)); // -> true, [2, 3]
+ */
