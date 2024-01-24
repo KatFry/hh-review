a73495b8c53c8ebb94c8ae65b981fd311822437c
Katherine Fry <katgfry@me.com>
2024-01-24 15:04:19 -0500

    3: question and examples

---
+++ 3-twoSum.js
@@ -0,0 +1,16 @@
+/* 
+Given an array of integers and a target integer, 
+return true if a pair of integers exist in the array that add up to the target. 
+Return false otherwise. 
+
+In second edition of the problem, return the indices of the numbers that add up to the target. 
+
+Examples: 
+twoSum([2, 4, 5], 9) // -> true, [1, 2]
+twoSum([2, 4, 5], 8) // -> false 
+twoSum([5, -1, -2, 3], 1) // -> true, [2, 3]
+*/
+
+const twoSum = (arr, target) => {
+  
+}
